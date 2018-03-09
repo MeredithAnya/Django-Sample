a8c71ce4b27d16bf16115b4eed8aeaa53606cd9a
Meredith <meredith@getsentry.com>
2018-03-09 10:30:34 -0800

    inital commit

---
+++ demo/myproject/settings/development.py
@@ -0,0 +1,1 @@
+MY_SENTRY_DSN = 'https://e5db5058067f492cb0d6af0b582a4e46:c624102267e846afaa1cea47e499e278@sentry.io/107169'
---
+++ demo/myproject/settings/production.py
@@ -0,0 +1,6 @@
+DEBUG = False
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
+# many Sentry users have a separate Project (with a unique DSN) for each environment
+MY_SENTRY_DSN = 'https://e5db5058067f492cb0d6af0b582a4e46:c624102267e846afaa1cea47e499e278@sentry.io/107169'
